7ba3f3de394f77ae9fc54145eb6616cc4d29ecc7
LUIZA OLIVEIRA ROSA <21192963@CFP127ALUNO.LOCAL>
2021-08-06 10:43:46 -0300

    Exercício 2 finalizado

--- seneletro/calcular.php
+++ seneletro/calcular.php
@@ -6,12 +6,14 @@ $consumo = $_POST["consumo"];
 
 if ($consumo > 120) {
     $valorTotal = $consumo * 0.42;
+    $class = "vermelho";
     $textoConsumo = "Consumo: $consumo kWh";
-    $texto = "";
+    $textoAgradecimento = "";
 } else {
     $valorTotal = $consumo * 0.32;
+    $class = "verde";
     $textoConsumo = "Consumo: $consumo kWh";
-    $texto = "Obrigado por economizar!";
+    $textoAgradecimento = "Obrigado por economizar!";
 }
 ?>
 
@@ -20,13 +22,14 @@ if ($consumo > 120) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Resultado</title>
 </head>
-<body>
+<body class = "<?= $class ?>">
     <h1>Conta de luz de <?= $nome ?></h1>
     <h3><?= $endereco ?></h3>
-    <h3><?= $textoConsumo ?></h3>
-    <h3>Valor a pagar: R$<?= number_format($valorTotal, 2) ?></h3>
-    <h3><?= $texto ?></h3>
+    <h3 class = "textoConsumo"><?= $textoConsumo ?></h3>
+    <h3>Valor a pagar: <h2> R$<?= number_format($valorTotal, 2) ?></h2></h3>
+    <h3 class = "textoAgradecimento"><?= $textoAgradecimento ?></h3>
 </body>
 </html>
